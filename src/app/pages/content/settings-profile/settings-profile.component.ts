f8adfacb84d12197ec749ecb93d2aaf44b10255a
Francelino <francelino.silva.prestserv@petrobras.com.br>
2024-10-28 16:36:37 -0300

    - Feat: adicionando tabela de lista de users

--- src/app/pages/content/settings-profile/settings-profile.component.ts
+++ src/app/pages/content/settings-profile/settings-profile.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import {
+  MatTableDataSource,
+  MatTableModule
+} from "@angular/material/table";
+import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
+import {User} from "../../../models/user";
 
 @Component({
   selector: 'app-settings-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule, MatPaginatorModule
+  ],
   templateUrl: './settings-profile.component.html',
   styleUrl: './settings-profile.component.scss'
 })
-export class SettingsProfileComponent {
+export class SettingsProfileComponent implements AfterViewInit{
+  ELEMENT_DATA: User[] = [];
+  displayedColumns: string[] = ['id', 'name', 'email'];
+  dataSource = new MatTableDataSource<User>(this.ELEMENT_DATA);
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
